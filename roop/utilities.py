0b0e0c30885536c40f6362dc98faa3ea070c7703
ylshie <yuliang.hsieh@gmail.com>
2023-09-04 10:38:29 +0800

    remove format

--- roop/utilities.py
+++ roop/utilities.py
@@ -59,7 +59,8 @@ def create_video(target_path: str, fps: float = 30) -> bool:
         commands.extend(['-crf', str(output_video_quality)])
     if roop.globals.output_video_encoder in ['h264_nvenc', 'hevc_nvenc']:
         commands.extend(['-cq', str(output_video_quality)])
-    commands.extend(['-pix_fmt', 'yuv420p', '-vf', 'colorspace=bt709:iall=bt601-6-625:fast=1', '-y', temp_output_path])
+    #commands.extend(['-pix_fmt', 'yuv420p', '-vf', 'colorspace=bt709:iall=bt601-6-625:fast=1', '-y', temp_output_path])
+    commands.extend(['-y', temp_output_path])
     print(commands)
     return run_ffmpeg(commands)
 
